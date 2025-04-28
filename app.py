0c4a01481f4f946b44b883f4b538717542f182b5
ma113804 <ma113804@student.sgh.waw.pl>
2025-04-28 17:16:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Witaj w moim API!"})
-@app.route('/api/v1.0/predict')
-def mojastrona(): 
+@app.route('/api/v1.0/predict', methods=['GET']')
+def predict(): 
     q = request.args.to_dict()
     if len(q) == 0 or len(q)==1:
         num1 = 0
         num2 = 0
         pred = 0
         pred_dict = {
-        'prediction': pred,
         'features': {
             'num1': num1,
             'num2': num2
-        }
+        }, 'prediction': pred
     }
         return jsonify(pred_dict)
     else:
@@ -28,11 +24,10 @@ def mojastrona():
         else:
             pred2=0
         pred_dict2 = pred_dict = {
-        'prediction': pred2,
         'features': {
             'num1': num1,
             'num2': num2
-        }
+        },'prediction': pred2
     }
         return jsonify(pred_dict2)
         
